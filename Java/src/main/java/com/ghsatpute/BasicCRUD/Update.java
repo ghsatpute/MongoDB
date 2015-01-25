6662afd024f97316ea12d3524f6e4954b10844ae
Ganesh Satpute <ghsatpute@gmail.com>
2015-01-25 23:30:49 +0530

    Added update operation code

--- Java/src/main/java/com/ghsatpute/BasicCRUD/Update.java
+++ Java/src/main/java/com/ghsatpute/BasicCRUD/Update.java
@@ -1,6 +1,13 @@
 package com.ghsatpute.BasicCRUD;
 
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import java.net.UnknownHostException;
+
 /*
  *  @author : Ganesh Satpute
  *  @date   : 1/18/2015
@@ -8,4 +15,23 @@ package com.ghsatpute.BasicCRUD;
  */
 public class Update {
 
+    public static void main(String[] args) throws UnknownHostException {
+        DBCollection collection = createDbCollection();
+        // Now update the some data
+        System.out.println("data" + collection.findOne(new BasicDBObject("_id",1)));
+        collection.update(new BasicDBObject("_id", 1), new BasicDBObject("data", "new data"));
+        System.out.println("new data" + collection.findOne(new BasicDBObject("_id",1)));
+
+    }
+
+    private static DBCollection createDbCollection() throws UnknownHostException{
+        MongoClient client = new MongoClient();
+        DB db = client.getDB("Update");
+        DBCollection collection = db.getCollection("test");
+        collection.drop();
+        for (int i = 0; i < 10; i++) {
+            collection.insert(new BasicDBObject().append("_id", i).append("data", "item_"+i));
+        }
+        return collection;
+    }
 }
